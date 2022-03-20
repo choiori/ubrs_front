f07db17f08e4d7d17aff9042c21dd154aaec212f
choiori <kjn0020@naver.com>
2022-03-20 14:56:16 +0900

    change

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  BASE_URL: 'https://localhost:5000',
+  BASE_URL: 'https://ubrs-server.herokuapp.com/',
   // headers: {
   //   'Content-Type': 'application/json',
   //},
